11f83070fd496e187eb596004d1b4e9e725d09e4
FelixEdenborgh <Felix@edenborgh.se>
2024-03-15 14:19:51 +0100

    Bytte bakgrund och la till så att stjärnorna kan vara på skrämen

--- 2dGame__SpaceShooter/Form1.Designer.cs
+++ 2dGame__SpaceShooter/Form1.Designer.cs
@@ -42,6 +42,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
             this.ClientSize = new System.Drawing.Size(584, 461);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(600, 500);
